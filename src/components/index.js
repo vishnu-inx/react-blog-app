7f3eb551680850a8ac8f736f025e14e07d2c9c5f
Vishnu Mevada <vishnu.m@inheritx.com>
2024-01-04 12:39:05 +0530

    implement components

--- src/components/index.js
+++ src/components/index.js
@@ -6,5 +6,10 @@ import LogoutBtn from "./Header/LogoutBtn";
 import Button from './Button';
 import Input from "./Input";
 import Select from "./Select";
+import Login from "./Login";
+import Signup from "./Signup";
+import AuthLayout from "./AuthLayout";
+import RTE from "./RTE";
+import PostForm from "./post-form/PostForm";
 
-export { Header, Footer, Container, Logo, LogoutBtn, Button, Input, Select };
+export { Header, Footer, Container, Logo, LogoutBtn, Button, Input, Select, Login, Signup, AuthLayout, RTE, PostForm };
