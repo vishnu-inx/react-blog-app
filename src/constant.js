b018b81ad11e39f6da57ec5a82e69832078523eb
Vishnu Mevada <vishnu.m@inheritx.com>
2024-01-05 18:50:44 +0530

    fix image logo issue

--- src/constant.js
+++ src/constant.js
@@ -2,20 +2,20 @@ export const blogData = [
     {
         id: 1,
         title: "Angular Blog",
-        image: "./src/assets/images/ng-blog.png"
+        image: "/src/assets/images/ng-blog.png"
     },
     {
         id: 2,
         title: "ReactJs Blog",
-        image: "./src/assets/images/react-blog.jpeg"
+        image: "/src/assets/images/react-blog.jpeg"
     },
     {
         id: 3,
         title: "HTML Blog",
-        image: "./src/assets/images/html-blog.png"
+        image: "/src/assets/images/html-blog.png"
     }
 ]
 
-export const logo = "./src/assets/images/blog-logo.png";
-export const mainBannerImage = "./src/assets/images/main-banner.jpg";
+export const logo = "/src/assets/images/blog-logo.png";
+export const mainBannerImage = "/src/assets/images/main-banner.jpg";
 export const skeletonItems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
